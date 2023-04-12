10ca1c07bc3e8ee934f96385b7ee98c55c30d6f6
nathan <kvngnathan8420@gmail.com>
2023-04-12 07:37:56 +0000

    Create css components

--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -1,13 +1,29 @@
-import './Task.css';
+import styled from 'styled-components';
+
 
 
 function Task({id, title , body , onDragStart}){
 
+    const TAskWrapper = styled.div`
+    
+        background: darkGray;
+        padding: 20px;
+        border-radius: 20px;
+        margin: 0% 5% 5% 5%;
+    
+    
+    h3 {
+        width: 100%;
+        margin: 0;
+    }
+    
+    `;
+
     return (
-        <div className="Task-wrapper" draggable onDragStart={(e)=> onDragStart(e , id)}>
+        <TAskWrapper draggable onDragStart={(e)=> onDragStart(e , id)}>
             <h3>{title}</h3>
             <p>{body}</p>
-        </div>
+        </TAskWrapper>
     )
 }
 
