fec49882acc983ded1318b40769dd0647f68b8d4
nathan <kvngnathan8420@gmail.com>
2023-04-13 00:21:07 +0000

    Add styled-components

--- src/pages/Board/Board.js
+++ src/pages/Board/Board.js
@@ -1,7 +1,8 @@
 import Lane from '../../components/Lane/Lane';
 import { useState, useEffect } from 'react';
 import useDataFetching from '../../hooks/useDataFetching';
-import './Board.css';
+import styled from 'styled-components';
+
 
 const lanes = [
   { id: 1, title: 'To Do' },
@@ -10,33 +11,58 @@ const lanes = [
   { id: 4, title: 'Done' },
 ];
 
-function onDragStart(e, id){
+function onDragStart(e, id) {
   e.dataTransfer.setData('id', id);
 }
 
-function onDragOver(e){
+function onDragOver(e) {
   e.preventDefault();
 }
 
 function Board() {
 
+
+  const BoardWrapper = styled.div`
+
+  @media (min-width: 768px) {
+  
+      display: flex;
+      justify-content: space-between;
+      flex-direction: row;
+      margin: 5%;
+      
+  }
+  
+  
+  @media (max-width: 768px) {
+    
+      display: flex;
+      width: 100% !important;
+      justify-content: space-between;
+      flex-direction: column !important;
+        
+      
+  }
+  
+  `;
+
   //using the custom hook to fetch data
-  const [ loading , error , data ] = useDataFetching(`https://my-json-server.typicode.com/PacktPublishing/React-Projects-Second-Edition/tasks`);
+  const [loading, error, data] = useDataFetching(`https://my-json-server.typicode.com/PacktPublishing/React-Projects-Second-Edition/tasks`);
 
-  const [ tasks , setTasks ] = useState([]);
+  const [tasks, setTasks] = useState([]);
 
-  useEffect(()=>{
+  useEffect(() => {
 
-      setTasks(data);
+    setTasks(data);
   }, [data]);
 
 
-  function onDrop(e, laneId){
+  function onDrop(e, laneId) {
 
     const id = e.dataTransfer.getData('id');
 
-    const updateTasks = tasks.filter((task)=>{
-      if(task.id.toString() === id){
+    const updateTasks = tasks.filter((task) => {
+      if (task.id.toString() === id) {
         task.lane = laneId;
       }
 
@@ -45,23 +71,23 @@ function Board() {
 
     setTasks(updateTasks);
   }
-  
+
 
   return (
-    <div className='Board-wrapper'>
+    <BoardWrapper>
       {lanes.map((lane) => (
-        <Lane key={lane.id} title={lane.title} 
-            loading={loading}  error = {error} 
-            laneId={lane.id} 
-            tasks = {tasks.filter((task)=>
-              task.lane === lane.id
-            )}
-            onDragStart={onDragStart}
-            onDragOver = {onDragOver}
-            onDrop={onDrop}
+        <Lane key={lane.id} title={lane.title}
+          loading={loading} error={error}
+          laneId={lane.id}
+          tasks={tasks.filter((task) =>
+            task.lane === lane.id
+          )}
+          onDragStart={onDragStart}
+          onDragOver={onDragOver}
+          onDrop={onDrop}
         />
       ))}
-    </div>
+    </BoardWrapper>
   );
 }
 
